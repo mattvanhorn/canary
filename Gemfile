487f398ea5171b2c760a196e7e04bd0fe6d3c3e7
Matt Van Horn <mattvh@scoopst.com>
2011-12-24 13:10:06 -0800

    stiil trying to get integriy on heroku

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'http://rubygems.org'
-gem 'bundler'
+gem 'bundler', '1.0.11'
 gem 'rails', '3.1.1'
 gem 'pg'
 gem 'jquery-rails'
--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,4 @@
 require 'rubygems'
-require 'bundler'
 
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
