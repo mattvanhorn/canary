fcce62a514a1d536c4589aa1a1eb16accd64f059
Matt Van Horn <mattvh@scoopst.com>
2012-01-02 22:21:27 -0800

    memcached sessions

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,11 @@
 # Be sure to restart your server when you modify this file.
-
-Canary::Application.config.session_store :cookie_store, key: '_canary_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Canary::Application.config.session_store :active_record_store
-
-require 'action_dispatch/middleware/session/dalli_store'
-Rails.application.config.session_store :dalli_store,
-                                        :memcache_server => ENV['MEMCACHE_SERVERS'].split(','),
-                                        :namespace => 'sessions',
-                                        :key => '_moodmetricsapp_session',
-                                        :expire_after => 30.minutes
+if Rails.env.production?
+  require 'action_dispatch/middleware/session/dalli_store'
+  Canary::Application.config.session_store :dalli_store,
+                                            :memcache_server => ENV['MEMCACHE_SERVERS'].split(','),
+                                            :namespace => 'sessions',
+                                            :key => '_moodmetricsapp_session',
+                                            :expire_after => 20.minutes
+else
+  Canary::Application.config.session_store :cookie_store, key: '_canary_session'
+end
