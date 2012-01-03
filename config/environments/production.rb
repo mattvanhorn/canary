9f2f5b8b66f189ae67db4d52698459798cd4bb3b
Matt Van Horn <mattvh@scoopst.com>
2012-01-02 22:33:24 -0800

    serve static assets with expires headers, I hope

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,6 +45,8 @@ Canary::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.serve_static_assets = true
+  config.static_cache_control = "public, max-age=3600"
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
